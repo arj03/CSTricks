906e7b738ef739c6343d07e9a6057b73c280e0ce
Anders Rune Jensen <anders@iola.dk>
2014-02-27 13:05:48 +0100

    Add method to get a unique password

--- CSTricks/CSTricks/PasswordHash.cs
+++ CSTricks/CSTricks/PasswordHash.cs
@@ -49,6 +49,24 @@ namespace PasswordHash
         public const int SALT_INDEX = 1;
         public const int PBKDF2_INDEX = 2;
 
+        public static string GetUniqueKey(int maxSize)
+        {
+            char[] chars = new char[62];
+            chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".ToCharArray();
+            byte[] data = new byte[1];
+            using (RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider())
+            {
+                crypto.GetNonZeroBytes(data);
+                data = new byte[maxSize];
+                crypto.GetNonZeroBytes(data);
+                StringBuilder result = new StringBuilder(maxSize);
+                foreach (byte b in data)
+                    result.Append(chars[b % (chars.Length)]);
+
+                return result.ToString();
+            }
+        }
+
         /// <summary>
         /// Creates a salted PBKDF2 hash of the password.
         /// </summary>
